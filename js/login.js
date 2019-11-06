a3954eef24ae863acf5a7cf77d329ca2c2103fc2
NadeemMasani <34688479+NadeemMasani@users.noreply.github.com>
2019-11-06 15:52:33 -0800

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,28 @@
+$("form").submit(function(event){
+    $("#error").empty();
+    event.preventDefault();
+    var emailid = $("#inputEmail").val();
+    var pwd = $("#inputPassword").val();
+$.ajax({
+        url: 'https://se532.herokuapp.com/login',
+        method: 'POST',
+        contentType : 'application/json',
+        dataType : 'json',
+        data: JSON.stringify({
+            'email': emailid,
+            'password': pwd
+        }),
+        success: function (data, status) {
+        console.log(data.success);
+        console.log(data.data);
+
+        if ( data.success === 0){
+          $("#error").append("Invalid credentials. Please try again");
+
+        }
+        else{
+            window.location.href = "success.html";
+        }
+    }
+})
+});
